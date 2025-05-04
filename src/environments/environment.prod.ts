37f39d3d616de49434976145edf1dd7268f70bfc
wendellvalnieri <wvalnieri@gmail.com>
2025-05-04 11:19:14 +0100

    Adicionar variável

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   appName: "Tamy Beleza",
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'http://localhost:3000/api',
+  prod_sulfix: 'prod',
 };
