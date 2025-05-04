1971707d19a651ebdf33457a27041d067096ee45
wendellvalnieri <wvalnieri@gmail.com>
2025-05-04 11:44:30 +0100

    Mudança de link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appName: "Tamy Beleza",
-  apiUrl: 'https://salao-nb66szilr-wendell-valnieris-projects.vercel.app',
+  apiUrl: 'https://salao-api.vercel.app',
   prod_sulfix: 'prod',
 };
